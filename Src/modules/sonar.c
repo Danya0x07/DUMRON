5058b7fb63a0e9bdeade591992b22edb793eae6d
Danya0x07 <danya9104449383@gmail.com>
2019-10-05 21:37:20 +0300

    Сонар работает нормально.

--- Src/modules/sonar.c
+++ Src/modules/sonar.c
@@ -1,8 +1,8 @@
 #include "modules/sonar.h"
 
-#define TRIGGER_PULSE_WIDTH    13
-#define ECHO_RESPONSE_TIMEOUT  60
-#define MAX_ECHO_PULSE_WIDTH   300
+#define TRIGGER_PULSE_WIDTH    12
+#define ECHO_RESPONSE_TIMEOUT  500
+#define MAX_ECHO_PULSE_WIDTH   11600
 
 #define sonar_trig_1()     LL_GPIO_SetOutputPin(SONAR_GPIO_Port, SONAR_TRIG_Pin)
 #define sonar_trig_0()     LL_GPIO_ResetOutputPin(SONAR_GPIO_Port, SONAR_TRIG_Pin)
@@ -26,5 +26,5 @@ uint16_t sonar_scan(void)
     sonar_timer_reset();
     while (sonar_echo_is_1() && sonar_timer_get() < MAX_ECHO_PULSE_WIDTH)
         ;
-    return sonar_timer_get();
+    return sonar_timer_get() / 58;
 }
